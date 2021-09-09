d259a9a8d5c2db20ff85084b2a570e1588bb204c
Mathieu Tuli <tuli.mathieu@gmail.com>
2021-09-09 11:01:56 -0400

    Remove a bunhc of junk

--- src/matutils/components.py
+++ src/matutils/components.py
@@ -8,20 +8,11 @@ class LogLevel(Enum):
     '''
     What the stdlib did not provide!
     '''
-    DEBUG = logging.DEBUG
     INFO = logging.INFO
-    WARNING = logging.WARNING
+    DEBUG = logging.DEBUG
     ERROR = logging.ERROR
+    WARNING = logging.WARNING
     CRITICAL = logging.CRITICAL
 
     def __str__(self):
         return self.name
-
-
-class Counter(dict):
-    """
-    Like a dict, but returns 0 if the key isn't found.
-    """
-
-    def __missing__(self, key: Any) -> int:
-        return 0
